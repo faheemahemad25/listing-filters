7946f9efd32dd6edb9b6f13c721ee11c89e127d6
Faheem Ahemad <faheemahemad25@gmail.com>
2025-12-31 10:51:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,18 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// // import './index.css'
-// import App from './App.jsx'
-// import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 
 
-// createRoot(document.getElementById('root')).render(
-//   <BrowserRouter basename="/listing-filters">
-//     <>
-//       <App />
-//     </>
-//   </BrowserRouter>
-
-// )
-
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HashRouter>
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/listing-filters">
+    <>
       <App />
-    </HashRouter>
-  </StrictMode>
-);
+    </>
+  </BrowserRouter>
+
+)
+
+
 
