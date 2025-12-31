58b4ef2ac62a636488580ea0fb33093492944330
Faheem Ahemadji <faheemahemad25@gmail.com>
2025-12-31 11:59:33 +0530

    delete layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,22 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// // import './index.css'
-// import App from './App.jsx'
-// import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 
-
-// createRoot(document.getElementById('root')).render(
-//   <BrowserRouter basename="/listing-filters">
-//     <>
-//       <App />
-//     </>
-//   </BrowserRouter>
-
-// )
-
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import { HashRouter } from "react-router-dom";
-
-// createRoot(document.getElementById('root')).render(
-//   <HashRouter>
-//     <App />
-//   </HashRouter>
-// )
-
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+  <BrowserRouter basename="/listing-filters">
+    <>
+      <App />
+    </>
+  </BrowserRouter>
+
+)
+
+
+
+
 
 
 
